1fbb44ce3f20b14a4f49e619eafcff3deb7d912c
Romario Marques Melo <romario.melos@gmail.com>
2022-04-18 10:53:39 -0300

    inclue novo arquivo pdf para analise

--- handler.py
+++ handler.py
@@ -1,15 +1,17 @@
 import json
 import boto3
+import os
 from extract_info import extract_info
 from cout_words import count_words_returns_dic
 from get_pdf import pdf_by_io, pdf_content_to_string
 
 def hello(event, context):
-    s3 = boto3.client('s3',
-        endpoint_url='http://localhost:4566',
-        region_name="us-east-1")
+
+    body_request = json.loads(event['body'])
+
+    s3 = get_s3_client()
     bucket_name = 'test-bucket'
-    file_name = 'votos/' + event['pathParameters']['filename']
+    file_name = body_request['key']
 
     s3_response_object = s3.get_object(Bucket=bucket_name, Key=file_name)
     object_content = s3_response_object['Body'].read()
@@ -22,7 +24,6 @@ def hello(event, context):
 
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!!!",
-        "arquivo": event['pathParameters']['filename'],
         "input": event,
     }
 
@@ -30,3 +31,14 @@ def hello(event, context):
 
     return buckets
 
+def get_python_env():
+    return os.getenv('PYTHON_ENV') or 'development'
+
+def get_s3_client():
+    if get_python_env() == 'development':
+        return boto3.client('s3',
+            endpoint_url='http://localhost:4566',
+            region_name="us-east-1")
+    else:
+        return boto3.client('s3',
+            region_name="us-east-1")
--- scripts/op-bucket.sh
+++ scripts/op-bucket.sh
@@ -3,3 +3,4 @@ BUCKET_NAME=test-bucket
 alias awslocal="aws --endpoint-url=http://localhost:4566"
 awslocal s3api create-bucket --bucket $BUCKET_NAME
 awslocal s3 cp 48500.006904-2019-52.pdf s3://$BUCKET_NAME/votos/
+awslocal s3 cp 48500.003446-2013-12.pdf s3://$BUCKET_NAME/votos/
